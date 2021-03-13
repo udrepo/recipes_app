8a9c3ce990f27493c58099766a231e52e2e2d5a3
ulan <ulan.0418@gmail.com>
2021-03-13 21:55:14 +0600

    initial commit

---
+++ lib/data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+const CATEGORIES = const[
+  Category(
+      id: 'c1',
+      title: 'Kazakh',
+      color: Colors.purple
+  ),
+  Category(
+      id: 'c1',
+      title: 'Russian',
+      color: Colors.white70
+  ),
+  Category(
+      id: 'c1',
+      title: 'English',
+      color: Colors.redAccent
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals menu',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          headline6: TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondenced',
+          )
+        )
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
